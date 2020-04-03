8fec7973bfe1816c48c5e01294f1e8fa006280b0
Ryan Kristoffer <ryank@peplink.com>
2020-04-03 13:40:18 +0800

    added collection items in view

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import CollectionItem from "../collection-item/collection-item.component";
+import "./collection-preview.styles.scss"
 
 const CollectionPreview = ({ title, items }) => {
-  console.log(items);
   return (
     <div className="collection-preview">
       <h1 className="title">{title}</h1>
       <div className="preview">
         {items
           .filter((item, idx) => idx < 4)
-          .map(item => (
-            <div>{item.name}</div>
+          .map(({ id, ...item }) => (
+            <CollectionItem key={id} {...item} />
           ))}
       </div>
     </div>
