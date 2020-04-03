e65fc52c7e6afe190b23f1c10b8b021827e570f0
Ryan Kristoffer <ryank@peplink.com>
2020-04-03 14:33:25 +0800

    added font tff format

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from "react-router-dom";
 import HatsPage from "./pages/hats/hatspage.component";
 import ShopPage from "./pages/shop/shoppage.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/signin-and-signup/signin-and-signup.component";
 
 function App() {
   return (
@@ -13,8 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
-        <Route exact path="/shop/hats" component={HatsPage} />
-        <Route path="/hats/:hatsId" component={HatsPage} />
+        <Route exact path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const SignInAndSignUpPage = () => <div>Sign in and signup</div>;
+
+export default SignInAndSignUpPage;
