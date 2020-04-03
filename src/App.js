2da7111fb25fd395c349c912fa7cd5db342e7335
Ryan Kristoffer <ryank@peplink.com>
2020-04-03 13:54:32 +0800

    added header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import HomePage from "./pages/homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
 import HatsPage from "./pages/hats/hatspage.component";
 import ShopPage from "./pages/shop/shoppage.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
